db1edcfff76c543948e420ce36694f8e89f4c04d
Subhro7805 <100530675+Subhro7805@users.noreply.github.com>
2026-05-08 11:20:13 -0500

    Update main.py

--- Wedge-Split/main.py
+++ Wedge-Split/main.py
@@ -147,7 +147,7 @@ boundary_conditions = [boundary.BoundaryCondition(domain, "loading", 1, facet_ta
 # phase-field boundary condition
 bc_z = boundary.bc_phasefield(domain, V_z, cracktip, outer)
 
-# Dirichlet boundary condition
+# Displacement boundary condition
 bc_u = boundary.bc_dirichlet(domain, V_u, boundary_conditions, rightbottom, bottom, loading, Xsym, Zsym)
 
 # Initialize the functions
